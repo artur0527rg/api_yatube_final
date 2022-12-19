17fc21528b253db43dd2b50fde8af92c51d5091b
ARTUR <rogozinskiyartur@gmail,com>
2022-12-19 19:34:01 +0300

    JWT connected

--- yatube_api/settings.py
+++ yatube_api/settings.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -88,6 +89,11 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 
 REST_FRAMEWORK = {        
         'DEFAULT_PERMISSION_CLASSES': [
-            'rest_framework.permissions.IsAuthenticated',
+            'rest_framework_simplejwt.authentication.JWTAuthentication',
         ],
+    }
+if DEBUG:
+    SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     }
